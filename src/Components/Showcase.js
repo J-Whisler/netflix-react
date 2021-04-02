825767bd47f30ddb3b7b5467c8aed3bcf1bff5ca
J-Whisler <jwhisler521@gmail.com>
2021-04-02 00:03:20 -0400

    Showcase content and styling

--- src/Components/Showcase.js
+++ src/Components/Showcase.js
@@ -4,6 +4,9 @@ import { motion } from "framer-motion";
 import Logo from "../img/logo.png";
 import Background from "../img/background.jpg";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 const Showcase = () => {
   return (
     <ShowcaseMain>
@@ -13,6 +16,17 @@ const Showcase = () => {
           Sign In
         </a>
       </ShowcaseTop>
+      <ShowcaseContent>
+        <h1>See what's next</h1>
+        <p>Watch anywhere. Cancel anytime</p>
+        <a href="" className="btn btn-xl">
+          Watch Free for 30 Days
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            className="icon"
+          ></FontAwesomeIcon>
+        </a>
+      </ShowcaseContent>
     </ShowcaseMain>
   );
 };
@@ -74,6 +88,51 @@ const ShowcaseTop = styled(motion.div)`
 
   .btn-rounded {
     border-radius: 0.3rem;
+  }
+`;
+
+const ShowcaseContent = styled(motion.div)`
+  position: relative;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: 9rem;
+  z-index: 2;
+  h1 {
+    font-weight: 700;
+    font-size: 3.2rem;
+    line-height: 1.1;
+    margin-bottom: 2rem;
+    letter-spacing: 0.2rem;
+  }
+  p {
+    text-transform: uppercase;
+    color: #fff;
+    font-weight: 400;
+    font-size: 1.7rem;
+    line-height: 1.25;
+    margin-bottom: 3rem;
+    letter-spacing: 0.1rem;
+  }
+  .btn-xl {
+    font-size: 1.5rem;
+    padding: 1.5rem 2.1rem;
+    text-transform: uppercase;
+    display: inline-block;
+    background: #e50914;
+    color: #fff;
+    padding: 1.5rem 2.1rem;
+    text-align: center;
+    border: none;
+    cursor: pointer;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+    border-radius: 0.1rem;
+    .icon {
+      margin-left: 1rem;
+    }
   }
 `;
 
